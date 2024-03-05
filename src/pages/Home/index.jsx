b83bced6eb5fcb68c7b160f0345d6ec147313855
Jocelito Oliveira Costa Junior <jocelito64@hotmail.com>
2024-03-05 17:47:07 -0300

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,30 @@
+import { FiTruck, FiTag, FiShoppingCart } from 'react-icons/fi';
+
+import { useAuth } from '../../hooks/auth';
+import { USER_ROLE } from '../../utils/roles'
+
+import { Container } from "./styles";
+import { Feature } from '../../components/Feature';
+import { Header } from '../../components/Header';
+
+export function Home() {
+  const { user } = useAuth();
+
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Feature title="Produto" icon={FiTag} to="/product" />
+
+        {
+          [USER_ROLE.ADMIN, USER_ROLE.SALE].includes(user.role) &&
+          <>
+            {user.role === USER_ROLE.ADMIN && <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" /> }
+            <Feature title="Relatório de vendas" icon={FiShoppingCart} to="/sales-report" />
+          </>
+        }
+      </main>
+    </Container>
+  )
+}
